6b68462e34cbeef471991ff9afb9a59335d7d439
ollinok <62589655+ollinok@users.noreply.github.com>
2022-08-09 22:54:21 +0300

    New Article Form basic functionality + moved components to subfolder

--- DataAccess/Data/ArticlesData.cs
+++ DataAccess/Data/ArticlesData.cs
@@ -33,17 +33,10 @@ public class ArticlesData : IArticlesData
         return article.FirstOrDefault();
     }
 
-    /*public Task<IEnumerable<FullArticlesModel>> GetFullArticleInfo(int id)
+    public async Task<ArticlesModel> CreateNewArticle()
     {
-        string sqlProcedure = @"select articles.created_at CreatedAt, articles.*, prices.price, prices.price_group_id PriceGroupId, prices.created_at CreatedAt 
-                                from articles
-                                left join prices on prices.article_id = articles.id
-                                where articles.id = @Id";
-        Func<FullArticlesModel, PricesModel, FullArticlesModel> func = (article, price) => { article.Price = price; return article; };
-        string splitOn = "updated_at";
-
-        return _db.LoadMultiMapSqlData<FullArticlesModel, PricesModel, dynamic>(sqlProcedure, new { Id = id }, func, splitOn);
-    }*/
+        return null;
+    }
 
     public async Task<IEnumerable<FullArticlesModel>> GetFullArticleInfo(int id)
     {
--- DataAccess/Models/ArticlesModel.cs
+++ DataAccess/Models/ArticlesModel.cs
@@ -1,8 +1,11 @@
-﻿namespace DataAccess.Models;
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace DataAccess.Models;
 
 public class ArticlesModel
 {
     public int Id { get; set; }
+    [RegularExpression(@"^[A-Z][a-z]{3,}\s\d+\smg\s\d+\stabl.$")]
     public string? Name { get; set; }
     public string? Ean { get; set; }
     public DateTime CreatedAt { get; set; }
