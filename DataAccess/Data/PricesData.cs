875f313b3afba715335333263903bfc3310095d7
ollinok <62589655+ollinok@users.noreply.github.com>
2022-08-11 14:27:12 +0300

    Article Prices editing

--- DataAccess/Data/PricesData.cs
+++ DataAccess/Data/PricesData.cs
@@ -16,7 +16,7 @@ public class PricesData : IPricesData
 
     public async Task<IEnumerable<FullPricesModel>> GetAllPricesByArticleId(int id)
     {
-        string sql = @"select prices.id, prices.price_group_id, prices.article_id, prices.price, prices.created_at CreatedAt, prices.updated_at UpdatedAt,
+        string sql = @"select prices.id, prices.price_group_id PriceGroupId, prices.article_id ArticleId, prices.price, prices.created_at CreatedAt, prices.updated_at UpdatedAt,
                               price_groups.id, price_groups.name
                        from prices
                        left join price_groups on price_groups.id = prices.price_group_id
@@ -29,6 +29,7 @@ public class PricesData : IPricesData
         return await _db.LoadMultiMapSqlData<FullPricesModel, PriceGroupsModel, dynamic>(sql, new { AId = id }, func, splitOn);
     }
 
+    // Turha?
     public async Task CreatePrice(int articleId, PricesModel price)
     {
         string sql = @"insert into prices (price_group_id, article_id, price, created_at)
@@ -47,6 +48,15 @@ public class PricesData : IPricesData
     {
         string sql = $@"insert into prices (price_group_id, article_id, price, created_at)
                        values (@PriceGroupId, {articleId}, @Price, CURRENT_TIMESTAMP)";
+        await _db.WriteSqlData(sql, prices);
+    }
+
+    public async Task UpdatePricesForArticle(List<FullPricesModel> prices)
+    {
+        string sql = @"INSERT INTO prices (id, price_group_id, article_id, price, created_at, updated_at)
+                       VALUES (@Id, @PriceGroupId, @ArticleId, @Price, CURRENT_TIMESTAMP, null)
+                       ON DUPLICATE KEY
+                       UPDATE price = @Price, updated_at = CURRENT_TIMESTAMP";
         await _db.WriteSqlData(sql, prices);
     }
 }
