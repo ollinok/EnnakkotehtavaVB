adf7bd89eb69b1fa30d21064a2ce2c6ed6baff43
ollinok <62589655+ollinok@users.noreply.github.com>
2022-08-14 18:17:44 +0300

    Added caching in DatabaseCount fetch

--- DataAccess/Data/DatabaseData.cs
+++ DataAccess/Data/DatabaseData.cs
@@ -1,18 +1,31 @@
-﻿namespace DataAccess.Data;
+﻿using Microsoft.Extensions.Caching.Memory;
+
+namespace DataAccess.Data;
 public class DatabaseData : IDatabaseData
 {
     private readonly ISqlAccess _db;
+    private readonly IMemoryCache _cache;
+    private const string cacheName = "CountsCache";
 
-    public DatabaseData(ISqlAccess db)
+    public DatabaseData(ISqlAccess db, IMemoryCache cache)
     {
         _db = db;
+        _cache = cache;
     }
 
     public async Task<DbRowCountModel?> GetTableCounts()
     {
-        string sql = @"SELECT COUNT(*) as ArticleCount FROM articles;
+        var results = _cache.Get<DbRowCountModel>(cacheName);
+        if (results == null)
+        {
+            string sql = @"SELECT COUNT(*) as ArticleCount FROM articles;
                        SELECT COUNT(*) as OrderCount FROM orders;
                        SELECT COUNT(*) as CustomerCount FROM customers;";
-        return await _db.LoadDatabaseCount(sql, new DbRowCountModel());
+            results = await _db.LoadDatabaseCount(sql, new DbRowCountModel());
+
+            _cache.Set(cacheName, results, TimeSpan.FromMinutes(1));
+        }
+
+        return results;
     }
 }
